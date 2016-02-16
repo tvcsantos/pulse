4762e347a5c584fd41dc7da1dd812574b321e7ca
Tiago Santos <tvcsantos@gmail.com>
2016-02-16 21:20:23 +0000

    Update Pulse.ino
    updated comments

--- Pulse.ino
+++ Pulse.ino
@@ -91,16 +91,17 @@ void loop() {
   // and the distance result in centimeters:
   long duration, cm;
 
-  // The PING))) is triggered by a HIGH pulse of 2 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+  // The PING))) is triggered by a HIGH pulse of 2 or more 
+  // microseconds. Give a short LOW pulse beforehand to 
+  // ensure a clean HIGH pulse:
   digitalWrite(PING_TRIG_PIN, LOW);
   delayMicroseconds(2);
   digitalWrite(PING_TRIG_PIN, HIGH);
   delayMicroseconds(10);
   digitalWrite(PING_TRIG_PIN, LOW);
 
-  // read the signal from the PING))): a HIGH
-  // pulse whose duration is the time (in microseconds) from the sending
+  // read the signal from the PING))): a HIGH pulse whose
+  // duration is the time (in microseconds) from the sending
   // of the ping to the reception of its echo off of an object.
   duration = pulseIn(PING_ECHO_PIN, HIGH);
 
